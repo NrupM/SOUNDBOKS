98e2180841e48982f2560db350cd4e6dcb40cd2e
NrupM <maryenorthrup@gmail.com>
2017-12-10 08:47:20 -0800

    add countdown color

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,24 +4,19 @@ const countDownDate = new Date("Dec 11, 2017 23:00:00").getTime();
 // Update the count down every 1 second
 const x = setInterval(function () {
 
-    // Get todays date and time
     const now = new Date().getTime();
 
-    // Find the distance between now an the count down date
     const distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in the element with id="demo"
     document.getElementById("countdown").innerHTML = `${hours}h ${minutes}m ${seconds}s`;
 
-    // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "SOLD OUT";
     }
+
 }, 1000);
